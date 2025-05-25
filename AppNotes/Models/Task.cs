e65eaa1b07b970e7c8807556274e49b6375da12d
Kkasperek01 <kkasperek0445@gmail.com>
2025-05-25 21:56:41 +0200

    Add files via upload

---
+++ AppNotes/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppNotes.Models
+{
+    public partial class Task : ObservableObject
+    {
+        [ObservableProperty]
+        public string name = "";
+
+        [ObservableProperty]
+        public bool isChecked = false;
+    }
+}
